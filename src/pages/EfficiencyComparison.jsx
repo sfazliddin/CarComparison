440bcd7d68896ba0de2398b4fb5a39407d533ac1
Samariddin <gotenks.ssj499@gmail.com>
2024-01-01 21:23:20 -0500

    made a form to add cars

--- src/pages/EfficiencyComparison.jsx
+++ src/pages/EfficiencyComparison.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 class Car {
-  constructor(make, model, year, cost, cityMPG, highwayMPG, combinedMPG) {
+  constructor(make, model, year, cost, cityMPG, highwayMPG) {
     this.make = make;
     this.model = model;
     this.year = year;
     this.cost = cost;
     this.cityMPG = cityMPG;
     this.highwayMPG = highwayMPG;
-    this.combinedMPG = combinedMPG;
+    this.combinedMPG = (cityMPG + highwayMPG) / 2;
   }
 }
 
@@ -20,8 +20,9 @@ const EfficiencyComparison = () => {
     cost: "",
     cityMPG: "",
     highwayMPG: "",
-    combinedMPG: "",
   });
+
+  const [carList, setCarList] = useState([]);
 
   const handleChange = (e) => {
     setCarInfo({ ...carInfo, [e.target.name]: e.target.value });
@@ -35,105 +36,121 @@ const EfficiencyComparison = () => {
       model,
       year,
       cost,
-      cityMPG,
-      highwayMPG,
+      parseInt(highwayMPG),
+      parseInt(cityMPG),
       combinedMPG
     );
 
-    // Example: Log the car information
-    console.log("Car Information:", car);
-
-    // Add your logic here to further use or display the car information
+    setCarList([...carList, car]);
+    setCarInfo({
+      make: "",
+      model: "",
+      year: "",
+      cost: "",
+      cityMPG: "",
+      highwayMPG: "",
+      combinedMPG: "",
+    });
   };
-  console.log;
+
   return (
     <>
-      <h1 style={{ alignSelf: "center" }}>Efficiency Comparison</h1>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          fontSize: "30px",
-          textAlign: "center",
-        }}
-      >
-        <div style={{ flex: "20%", padding: "5px" }}>
-          <h3> Car 1</h3>
-          <form>
-            <label htmlFor="make">Make:</label>
-            <input
-              type="text"
-              id="make"
-              name="make"
-              value={carInfo.make}
-              onChange={handleChange}
-              required
-            />
+      <div style={{ textAlign: "center" }}>
+        <h1>Car Information Form</h1>
 
-            <label htmlFor="model">Model:</label>
-            <input
-              type="text"
-              id="model"
-              name="model"
-              value={carInfo.model}
-              onChange={handleChange}
-              required
-            />
+        <form>
+          <label htmlFor="make">Make:</label>
+          <input
+            type="text"
+            id="make"
+            name="make"
+            value={carInfo.make}
+            onChange={handleChange}
+            required
+          />
 
-            <label htmlFor="year">Year:</label>
-            <input
-              type="number"
-              id="year"
-              name="year"
-              value={carInfo.year}
-              onChange={handleChange}
-              required
-            />
+          <label htmlFor="model">Model:</label>
+          <input
+            type="text"
+            id="model"
+            name="model"
+            value={carInfo.model}
+            onChange={handleChange}
+            required
+          />
 
-            <label htmlFor="cost">Cost:</label>
-            <input
-              type="number"
-              id="cost"
-              name="cost"
-              value={carInfo.cost}
-              onChange={handleChange}
-              required
-            />
+          <label htmlFor="year">Year:</label>
+          <input
+            type="number"
+            id="year"
+            name="year"
+            value={carInfo.year}
+            onChange={handleChange}
+            required
+          />
 
-            <label htmlFor="cityMPG">City MPG:</label>
-            <input
-              type="number"
-              id="cityMPG"
-              name="cityMPG"
-              value={carInfo.cityMPG}
-              onChange={handleChange}
-              required
-            />
+          <label htmlFor="cost">Cost:</label>
+          <input
+            type="number"
+            id="cost"
+            name="cost"
+            value={carInfo.cost}
+            onChange={handleChange}
+            required
+          />
 
-            <label htmlFor="highwayMPG">Highway MPG:</label>
-            <input
-              type="number"
-              id="highwayMPG"
-              name="highwayMPG"
-              value={carInfo.highwayMPG}
-              onChange={handleChange}
-              required
-            />
+          <label htmlFor="cityMPG">City MPG:</label>
+          <input
+            type="number"
+            id="cityMPG"
+            name="cityMPG"
+            value={carInfo.cityMPG}
+            onChange={handleChange}
+            required
+          />
 
-            <label htmlFor="combinedMPG">Combined MPG:</label>
-            <input
-              type="number"
-              id="combinedMPG"
-              name="combinedMPG"
-              value={carInfo.combinedMPG}
-              onChange={handleChange}
-              required
-            />
+          <label htmlFor="highwayMPG">Highway MPG:</label>
+          <input
+            type="number"
+            id="highwayMPG"
+            name="highwayMPG"
+            value={carInfo.highwayMPG}
+            onChange={handleChange}
+            required
+          />
 
-            <button type="button" onClick={submitForm}>
-              Submit
-            </button>
-          </form>
+          <button type="button" onClick={submitForm}>
+            Submit
+          </button>
+        </form>
+
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {carList.map((car, index) => (
+            <div key={index} style={{ width: "25%", padding: "10px" }}>
+              <h3>Car {index + 1}</h3>
+              <p>
+                <strong>Make:</strong> {car.make}
+              </p>
+              <p>
+                <strong>Model:</strong> {car.model}
+              </p>
+              <p>
+                <strong>Year:</strong> {car.year}
+              </p>
+              <p>
+                <strong>Cost:</strong> ${car.cost}
+              </p>
+              <p>
+                <strong>City MPG:</strong> {car.cityMPG}
+              </p>
+              <p>
+                <strong>Highway MPG:</strong> {car.highwayMPG}
+              </p>
+              <p>
+                <strong>Combined MPG:</strong> {car.combinedMPG}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </>
